2215002ea45d67c0ae2d9e4d922ee4b9e40a3807
isaac0A0 <isaac.wuyuz@gmail.com>
2020-12-08 21:28:42 -0800

    hw3 added

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,18 +1,23 @@
 # object representation
-from PIL import Image, ImageDraw
+import cv2
+import numpy as np
 import math
 import csv
 
-vert = False
+vert = True
 IMGSIZE = 600
-IMGMID = IMGSIZE / 2
-img = Image.new('RGB', (IMGSIZE, IMGSIZE))
-
+IMGMID = int(IMGSIZE / 2)
+d = 2
+amp = IMGSIZE
+sens = 0.005
+pixels = np.zeros((IMGSIZE, IMGSIZE, 3), np.uint8)
 # this list hold all vertices
 # you can use append() to add all x, y, z
 # into it. the format could be like this:
 # [[x1, y1, z1], [x2, y2, z2],......]
 vertices = []
+indexes = []
+
 def rotateX(theta):
     cos = math.cos(theta)
     sin = math.sin(theta)
@@ -43,26 +48,31 @@ with open('face-vertices.data') as csv_file:
 
     for row in csv_reader:    
         vertices.append([float(x) for x in row])
-        #print(float(row[0]), float(row[1]), float(row[2]))
 
-pixels = img.load()
+#read in face indexes
+with open('face-index.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
 
-def draw_pixel(x,y, color):
-    if x >= 0 and x <= IMGSIZE and y >= 0 and y <= IMGSIZE:
-        pixels[x,y] = (color,0,0)
+        for index in csv_reader:
+            indexes.append(index)
 
-d = 2
-amp = IMGSIZE
+def clean():
+    global pixels
+    pixels = np.zeros((IMGSIZE, IMGSIZE, 3), np.uint8)
+
+def draw_pixel(x, y, color):
+    if x >= 0 and x < IMGSIZE and y >= 0 and y < IMGSIZE:
+        pixels[int(y)][int(x)][0] = color
+
 def plotvert():
     for verticy in vertices:
         zbd = float(verticy[2]) / d
-        xp = int(amp * float(verticy[0]) / (1.0 + zbd))
-        yp = int(amp * float(verticy[1]) / (1.0 - zbd))
+        xp = int(amp * verticy[0] / (1.0 + zbd))
+        yp = int(amp * verticy[1] / (1.0 - zbd))
         #print(xp, yp)
         draw_pixel(IMGMID + xp, IMGMID - yp, 255)
-def prottri(a, b, c):
-    draw = ImageDraw.Draw(img)
 
+def prottri(a, b, c):
     zbda = vertices[a][2] / d
     xpa = int(amp * vertices[a][0] / (1.0 + zbda))
     ypa = int(amp * vertices[a][1] / (1.0 - zbda))
@@ -73,24 +83,77 @@ def prottri(a, b, c):
     xpc = int(amp * vertices[c][0] / (1.0 + zbdc))
     ypc = int(amp * vertices[c][1] / (1.0 - zbdc))
 
+    cv2.line(pixels, (IMGMID + xpa, IMGMID - ypa), (IMGMID + xpb, IMGMID - ypb), (255, 0, 0))
+    cv2.line(pixels, (IMGMID + xpa, IMGMID - ypa), (IMGMID + xpc, IMGMID - ypc), (255, 0, 0))
+    cv2.line(pixels, (IMGMID + xpc, IMGMID - ypc), (IMGMID + xpb, IMGMID - ypb), (255, 0, 0))
 
-    draw.line((IMGMID + xpa,IMGMID - ypa,IMGMID + xpb,IMGMID - ypb), fill=255)
-    draw.line((IMGMID + xpa,IMGMID - ypa,IMGMID + xpc,IMGMID - ypc), fill=255)
-    draw.line((IMGMID + xpc,IMGMID - ypc,IMGMID + xpb,IMGMID - ypb), fill=255)
+def plotlines():
+    for index in indexes:
+        prottri(int(index[0]), int(index[1]), int(index[2]))
 
-#rotateX((180+45) / 180 * math.pi)
-#rotateY(45 / 180 * math.pi)
-if vert:
-    plotvert()
-else:
-    #read index
-    with open('face-index.txt') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
+def refresh():
+    clean()
+    cv2.putText(pixels, "hold left mouse buttom to rotate", (0,10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+    cv2.putText(pixels, "hold right mouse buttom and move up/down to zoom", (0,24), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+    cv2.putText(pixels, "press c to change view", (0,38), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+    cv2.putText(pixels, "press esc to change escape", (0,52), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+    if vert:
+        plotvert()
+    else:
+        plotlines()
+    cv2.imshow("image", pixels)
 
-        for index in csv_reader:
-            prottri(int(index[0]), int(index[1]), int(index[2]))
+prevxl = -1 
+prevyl = -1
+prevyr = -1
+def rotate_and_zoom(event, x, y, flags, param):
+    global prevxl
+    global prevyl
+    global prevyr
+    global amp
+    if vert:        #limits the threashold to refresh to reduce computation
+        th = 40
+    else:
+        th = 60
+    if event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON :
+        if prevxl != -1:
+            xdiffl = prevxl - x
+            ydiffl = prevyl - y
+            if abs(xdiffl) + abs(ydiffl) > th:
+                rotateX(sens * -(ydiffl))
+                rotateY(sens * -(xdiffl))
+                refresh()
+                prevxl = x
+                prevyl = y
+        else:
+            prevxl = x
+            prevyl = y
+    if event == cv2.EVENT_LBUTTONUP:
+        prevxl = -1
+        prevyl = -1
+    if event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_RBUTTON :
+        if prevyr != -1:
+            ydiffr = prevyr - y
+            if abs(ydiffr) > th/3:
+                amp = amp * math.e**(0.005 * ydiffr)
+                refresh()
+                prevyr = y
+        else:
+            prevyr = y
+    if event == cv2.EVENT_RBUTTONUP:
+        prevyr = -1
         
+rotateX((-15) / 180 * math.pi)
+rotateY(-30 / 180 * math.pi)
 
+refresh()
+cv2.setMouseCallback("image", rotate_and_zoom)
 
-img.show()
+while True:
+    k = cv2.waitKey(200)
+    if k == 27:     #esc
+        break
+    if k == 99 or k == 67:    #'c' or 'C'
+        vert = not vert
+        refresh()
+cv2.destroyAllWindows()
---
+++ hw3/hw3.py
@@ -0,0 +1,222 @@
+import numpy as np
+from PIL import Image
+from math import sqrt
+
+MAXITER = 6
+SAVE = True
+WIDTH = 320
+HEIGHT = 240
+SCREENDIS = 1
+PICLEN = 0.001
+FPFIXER = 1e-11
+ENABLEREFLECT = False
+
+class pos:
+    def __init__(self, x, y, z):
+        self.arr = np.array([x, y, z], dtype = float)
+
+    def __add__(self, other):
+        arr = self.arr + other.arr
+        return pos(arr[0], arr[1], arr[2])
+
+    def __sub__(self, other):
+        arr = self.arr - other.arr
+        return pos(arr[0], arr[1], arr[2])
+
+    def __mul__(self, other):
+        arr = np.multiply(self.arr, other)
+        return pos(arr[0], arr[1], arr[2])
+
+    def dot(self, other):
+        return np.dot(self.arr, other.arr)
+
+    def length(self):
+        return sqrt(np.dot(self.arr, self.arr))
+
+    def cosine(self, other):
+        return self.dot(other) / (self.length() * other.length())
+
+    def reflect(self, axis):
+        L = self.arr
+        N = axis.arr
+        arr = (2 * self.dot(axis)) * N - L
+        return pos(arr[0], arr[1], arr[2])
+
+class color:
+    def __init__(self, r, g, b):
+        self.arr = np.array([r, g, b], dtype = int)
+
+    def __add__(self, other):
+        arr = self.arr + other.arr
+        for i in range(3):
+            if arr[i] > 255:
+                arr[i] = 255
+        return color(arr[0], arr[1], arr[2])
+
+    def dimm(self, other):
+        arr = np.array([0, 0, 0], dtype = int)
+        for i in range(3):
+            arr[i] = int(self.arr[i] * other.arr[i] / 255)
+        return color(arr[0], arr[1], arr[2])
+
+    def __mul__(self, other):
+        arr = np.multiply(self.arr, other)
+        for i in range(3):
+            if arr[i] < 0:
+                arr[i] = 0
+        return color(arr[0], arr[1], arr[2])
+
+    def __eq__(self, other):
+        return self.arr.all() == other.arr.all()
+
+class instance:
+    def __init__(self, x, y, z):
+        self.pos = pos(x, y, z)
+
+class light(instance):
+    def __init__(self, x, y, z, r, g, b):
+        super().__init__(x, y, z)
+        self.color = color(r, g, b)
+
+class sphere(instance):
+    def __init__(self, x, y, z,
+                 r, g, b,
+                 rad = 1, shine = 8,
+                 kar = 200, kag = 200, kab = 200,
+                 kdr = 150, kdg = 150, kdb = 150,
+                 ksr = 30, ksg = 30, ksb = 30, reflect = 0):
+        super().__init__(x, y, z)
+        self.color = color(r, g, b)
+        self.radius = rad
+        self.shine = shine
+        self.ka = color(kar, kag, kab)
+        self.kd = color(kdr, kdg, kdb)
+        self.ks = color(ksr, ksg, ksb)
+        self.reflect = reflect
+
+    def normal(self, pos):
+        return pos - self.pos
+
+    def intersect(self, o, d):      # find t for o + td
+        a = d.dot(d)
+        tmp = o - self.pos
+        b = (tmp * 2).dot(d)
+        c = tmp.dot(tmp) - self.radius ** 2
+
+        det = b ** 2 - 4 * a * c
+        if det < 0:
+            return
+        t1 = (-sqrt(det) - b) / (2 * a)
+        t2 = (sqrt(det) - b) / (2 * a)
+
+        if t2 < 0:
+            return
+        if t1 < 0:
+            return t2
+        return min(t1, t2)
+
+
+BLACK = color(0, 0, 0)
+Ia = color(20, 20, 20)
+
+def finddir(w, h, e):
+    m_x = int(WIDTH / 2)
+    m_y = int(HEIGHT / 2)
+    p = pos((w - m_x) * PICLEN, SCREENDIS, (m_y - h) * PICLEN)
+    p = p - e.pos
+    return(p)
+
+def max3(a, b, c):
+    if a > b:
+        if a > c:
+            return a
+        return c
+    elif b > c:
+        return b
+    return c
+
+def hits(source, dir, spheres):
+    if spheres is None:
+        return False
+    for s in spheres:
+        if s.intersect(source, dir):
+            return True
+    return False
+
+def raycast(source, dir, spheres, lights, n = 0):
+    if n == MAXITER:
+        return BLACK
+
+    c = BLACK
+    min = float("inf")
+    sph = None
+
+    if spheres is None:
+        return BLACK
+
+    for s in spheres:
+        pos = s.intersect(source, dir)
+        if pos is not None and pos < min:
+            min = pos
+            sph = s
+
+    if sph is not None:
+        intersection = source + dir * (min - FPFIXER)
+        norm = sph.normal(intersection)
+        Illu = BLACK
+        for li in lights:
+            ldir = li.pos - intersection
+
+            if hits(intersection, ldir, spheres):
+                Ie = BLACK
+            else:
+                Ie = li.color
+
+            Illa = Ia.dimm(sph.ka)
+            Illd = Ie.dimm(sph.kd) * ldir.cosine(norm)
+            ref = ldir.reflect(norm)
+            cos_value = (dir * -1).cosine(ref)
+            if cos_value < 0:
+                cos_value = 0
+            Ills = Ie.dimm(sph.ks) * (cos_value ** sph.shine)
+            Illu = Illu + Illa + Illd + Ills
+        reflection = BLACK
+
+        reflection = raycast(intersection, dir.reflect(norm) * -1, spheres, lights, n + 1)
+
+        c = sph.color.dimm(Illu) + reflection * sph.reflect
+    return(c)
+
+def drawpix(p, w, h, c):
+    p[w, h] = tuple(c.arr)
+
+
+if __name__ == "__main__":
+    img = Image.new('RGB', (WIDTH, HEIGHT))
+    pixels = img.load()
+    eye = instance(0, 0, 0)
+    spheres = []
+    lights = []
+    if ENABLEREFLECT:
+        spheres.append(sphere(-1.3, 20, 1, 255, 0, 0, 1, reflect = 0.7))
+        spheres.append(sphere(1.3, 20, 1, 0, 0, 255, 1, reflect = 0.7))
+        spheres.append(sphere(0, 20, -1, 0, 255, 0, 1, reflect = 0.7))
+        lights.append(light(0, 0, 0, 255, 255, 255))
+    else:
+        spheres.append(sphere(-1, 20, 0, 255, 255, 255, 1))
+        spheres.append(sphere(0.3, 19, 1, 0, 0, 255, 0.5))
+        lights.append(light(70, -50, 30, 255, 255, 255))
+
+    # spheres.append(sphere(0, 20, 0, 0, 0, 255, 1))
+    # lights.append(light(0, -25, 0, 30, 30, 30))
+    for h in range(HEIGHT):
+        for w in range(WIDTH):
+            direction = finddir(w, h, eye)
+            c = raycast(eye.pos, direction, spheres, lights)
+            drawpix(pixels, w, h, c)
+    img.show()
+    if SAVE:
+        if ENABLEREFLECT:
+            img.save("ratcast_ref.png")
+        else:
+            img.save("ratcast.png")
